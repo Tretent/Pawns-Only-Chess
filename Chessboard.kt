3f947e689e580948060c2392272b81d6d83a8079
Roberto Pasquali <pasquali.roberto@gmail.com>
2022-09-28 11:58:13 +0200

    4/5 - Capture

--- Chessboard.kt
+++ Chessboard.kt
@@ -4,6 +4,8 @@ const val CHESS_COLUMN_NUMBER = 8
 const val CHESS_ROWS_NUMBER = 8
 const val WHITE_PAWNS_STARTING_RANK = 2
 const val BLACK_PAWNS_STARTING_RANK = 7
+const val WHITE_PAWNS_EN_PASSANT_RANK = 5
+const val BLACK_PAWNS_EN_PASSANT_RANK = 4
 
 enum class SquareType(val representation: String) {
     WHITE(" W "), BLACK(" B "), EMPTY("   ")
@@ -24,6 +26,8 @@ class Chessboard {
             }
         }
     }
+    private var whiteFirstMove2Squares = false
+    private var blackFirstMove2Squares = false
 
     fun readPlayerNames(): List<String> {
         println("First Player's name:")
@@ -99,15 +103,14 @@ class Chessboard {
                     break
                 }
 
-                else -> if (isValidMove(command, color)) {
-                    updateChessboard(command, color)
-                    break
-                }
+                else -> if (isValidMove(command, color)) break
             }
         }
     }
 
-    private fun updateChessboard(playerInput: String, color: SquareType) {
+    private fun squareColor(rank: Int, file: Int): SquareType = chessboard[rank - 1][file]
+
+    private fun updateChessboard(playerInput: String, color: SquareType, isEnPassant: Boolean = false) {
         val startingFile = playerInput[0] - 'a'
         val startingRank = playerInput[1].digitToInt()
         val endingFile = playerInput[2] - 'a'
@@ -115,6 +118,8 @@ class Chessboard {
 
         chessboard[startingRank - 1][startingFile] = SquareType.EMPTY
         chessboard[endingRank - 1][endingFile] = color
+        if (isEnPassant && color == SquareType.WHITE) chessboard[endingRank - 2][endingFile] = SquareType.EMPTY
+        else if (isEnPassant && color == SquareType.BLACK) chessboard[endingRank][endingFile] = SquareType.EMPTY
     }
 
     private fun isValidMove(playerInput: String, color: SquareType): Boolean {
@@ -125,13 +130,61 @@ class Chessboard {
         val endingRank = playerInput[3].digitToInt()
 
         val sameFile = startingFile == endingFile
-        val emptySquare = chessboard[endingRank - 1][endingFile] == SquareType.EMPTY
-        val firstWhiteMove =
+        val emptyEndingSquare = squareColor(endingRank, endingFile) == SquareType.EMPTY
+        val whiteEndingSquare = squareColor(endingRank, endingFile) == SquareType.WHITE
+        val blackEndingSquare = squareColor(endingRank, endingFile) == SquareType.BLACK
+
+        val whiteFirstMove =
             startingRank == WHITE_PAWNS_STARTING_RANK && (endingRank == WHITE_PAWNS_STARTING_RANK + 1 || endingRank == WHITE_PAWNS_STARTING_RANK + 2)
         val whiteMove = startingRank != WHITE_PAWNS_STARTING_RANK && endingRank == startingRank + 1
-        val firstBlackMove =
+        val whiteStandardCapture =
+            if (startingRank == CHESS_ROWS_NUMBER - 1) false
+            else if (startingFile == 0) endingRank == startingRank + 1 && endingFile == startingFile + 1
+            else if (startingFile == CHESS_COLUMN_NUMBER - 1) endingRank == startingRank + 1 && endingFile == startingFile - 1
+            else endingRank == startingRank + 1 && (endingFile == startingFile + 1 || endingFile == startingFile - 1)
+        val whiteEnPassantCapture = blackFirstMove2Squares && startingRank == WHITE_PAWNS_EN_PASSANT_RANK &&
+                when (startingFile) {
+                    0 -> squareColor(startingRank, startingFile + 1) == SquareType.BLACK &&
+                            endingRank == startingRank + 1 && endingFile == startingFile + 1
+
+                    CHESS_COLUMN_NUMBER - 1 -> squareColor(startingRank, startingFile - 1) == SquareType.BLACK &&
+                            endingRank == startingRank + 1 && endingFile == startingFile - 1
+
+                    else -> endingRank == startingRank + 1 &&
+                            (squareColor(startingRank, startingFile + 1) == SquareType.BLACK &&
+                                    endingFile == startingFile + 1) ||
+                            (squareColor(startingRank, startingFile - 1) == SquareType.BLACK &&
+                                    endingFile == startingFile - 1)
+                }
+        val whiteLegitMove = sameFile && emptyEndingSquare && (whiteFirstMove || whiteMove)
+        val whiteLegitCapture =
+            (blackEndingSquare && whiteStandardCapture) || (emptyEndingSquare && whiteEnPassantCapture)
+
+        val blackFirstMove =
             startingRank == BLACK_PAWNS_STARTING_RANK && (endingRank == BLACK_PAWNS_STARTING_RANK - 1 || endingRank == BLACK_PAWNS_STARTING_RANK - 2)
         val blackMove = startingRank != BLACK_PAWNS_STARTING_RANK && endingRank == startingRank - 1
+        val blackStandardCapture =
+            if (startingRank == 0) false
+            else if (startingFile == 0) endingRank == startingRank - 1 && endingFile == startingFile + 1
+            else if (startingFile == CHESS_COLUMN_NUMBER - 1) endingRank == startingRank - 1 && endingFile == startingFile - 1
+            else endingRank == startingRank - 1 && (endingFile == startingFile + 1 || endingFile == startingFile - 1)
+        val blackEnPassantCapture = whiteFirstMove2Squares && startingRank == BLACK_PAWNS_EN_PASSANT_RANK &&
+                when (startingFile) {
+                    0 -> squareColor(startingRank, startingFile + 1) == SquareType.WHITE &&
+                            endingRank == startingRank - 1 && endingFile == startingFile + 1
+
+                    CHESS_COLUMN_NUMBER - 1 -> squareColor(startingRank, startingFile - 1) == SquareType.WHITE &&
+                            endingRank == startingRank - 1 && endingFile == startingFile - 1
+
+                    else -> endingRank == startingRank - 1 &&
+                            (squareColor(startingRank, startingFile + 1) == SquareType.WHITE &&
+                                    endingFile == startingFile + 1) ||
+                            (squareColor(startingRank, startingFile - 1) == SquareType.WHITE &&
+                                    endingFile == startingFile - 1)
+                }
+        val blackLegitMove = sameFile && emptyEndingSquare && (blackFirstMove || blackMove)
+        val blackLegitCapture =
+            (whiteEndingSquare && blackStandardCapture) || (emptyEndingSquare && blackEnPassantCapture)
 
         if (!validSquareRegexp.matches(playerInput)) println("Invalid Input").also { return false }
 
@@ -140,20 +193,25 @@ class Chessboard {
                 return if (chessboard[startingRank - 1][startingFile] != SquareType.WHITE) {
                     println("No white pawn at ${'a' + startingFile}$startingRank")
                     false
-                } else if (sameFile && (firstWhiteMove || whiteMove) && emptySquare) {
+                } else if (whiteLegitMove || whiteLegitCapture) {
+                    whiteFirstMove2Squares =
+                        startingRank == WHITE_PAWNS_STARTING_RANK && endingRank == WHITE_PAWNS_STARTING_RANK + 2 && sameFile
+                    updateChessboard(playerInput, SquareType.WHITE, whiteEnPassantCapture)
                     true
                 } else {
                     println("Invalid Input")
                     false
                 }
-
             }
 
             SquareType.BLACK -> {
                 return if (chessboard[startingRank - 1][startingFile] != SquareType.BLACK) {
                     println("No black pawn at ${'a' + startingFile}$startingRank")
                     false
-                } else if (sameFile && (firstBlackMove || blackMove) && emptySquare) {
+                } else if (blackLegitMove || blackLegitCapture) {
+                    blackFirstMove2Squares =
+                        startingRank == BLACK_PAWNS_STARTING_RANK && endingRank == BLACK_PAWNS_STARTING_RANK - 2 && sameFile
+                    updateChessboard(playerInput, SquareType.BLACK, blackEnPassantCapture)
                     true
                 } else {
                     println("Invalid Input")
